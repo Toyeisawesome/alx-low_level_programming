0c1dde07a34b7f3c8e5892cf2ceb2be52a8e7111
Toyeisawesome <toyeisawesome@gmail.com>
2022-03-14 14:49:20 -0700

    code2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - lowercase alphabet
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char lowerCase;
+
+	lowercase = 'a';
+
+	while (lowercase<= 'z')
+	{
+		putchar(lowerCase);
+		lowerCase++;
+	}
+	putchar('\n');
+	return (0);
+}
