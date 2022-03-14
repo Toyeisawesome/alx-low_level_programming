2794be14c1ff98ff2f4e068aa9d316ff2a9bba13
Toyeisawesome <toyeisawesome@gmail.com>
2022-03-14 15:08:22 -0700

    code 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - lowercase and uppercase alpha bet
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char lowerCase, upperCase;
+
+	lowerCase = 'a';
+	upperCase = 'A';
+
+	while (lowerCase <= 'z')
+	{
+		putchar (lowerCase);
+		lowerCase++;
+	}
+	while (uppercase <= 'Z')
+	{
+		putchar (upperCase);
+		upperCase++;
+	}
+	putchar ('\n');
+	return (0);
+}
