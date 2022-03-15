f88b0b81dca1abb46ffb43e4b37046ab190733e3
Toyeisawesome <toyeisawesome@gmail.com>
2022-03-15 07:01:04 -0700

    firstt2

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-/** main - this program is to print _putchar
+/**
+ * main - this program is to print _putchar
  *
  * Return: (0)
  */
 int main(void)
 {
+
+	int _putchar;
+
 	printf("_putchar\n");
 	return (0);
 }
