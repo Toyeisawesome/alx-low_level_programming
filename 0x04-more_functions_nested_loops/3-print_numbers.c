5e59a6bb225019fc48fa676aa40749e9eafa8965
Toyeisawesome <toyeisawesome@gmail.com>
2022-03-17 17:51:01 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "main.h"
+
+/**
+ * main - a function that prints the numbers, from 0 to 9
+ *
+ * Return: 0-9 followed by a newline
+ *
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		_putchar(i + '0');
+	_putchar('\n');
+}
