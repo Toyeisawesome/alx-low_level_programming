06ab32968dcb8af0228a8555de5fa5251cef7689
Toyeisawesome <toyeisawesome@gmail.com>
2022-03-21 10:59:31 -0700

    commit 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "main.h"
+
+/**
+ * main - give the length of a string
+ * @s: the string
+ *
+ * Return: the length of a string
+ *
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0' ; i++);
+	return (i);
+}
