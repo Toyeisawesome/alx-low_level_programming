caa23f67b684e8ef13350d273bb4ca6dbd88ffad
Toyeisawesome <toyeisawesome@gmail.com>
2022-03-21 10:03:10 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 #include "main.h"
 
 /**
- * main - takes a pointer to an int as parameter
+ * main - first task
  *
  * n: takes the value
  *
@@ -11,5 +11,6 @@
 void reset_to_98();
 void reset_to_98(int *n)
 {
+	int *n;
 	*n = 98;
 }
