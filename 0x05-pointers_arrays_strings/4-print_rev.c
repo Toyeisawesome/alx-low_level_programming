d2cac87543dc1502ba903bb287929a0625d71289
Toyeisawesome <toyeisawesome@gmail.com>
2022-03-21 13:51:27 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "main.h"
+
+/**
+ * main - prints a string in reverse
+ * @s: string
+ *
+ * Return: a string in reverse 
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	while (i > 0)
+	{
+		i--;
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
