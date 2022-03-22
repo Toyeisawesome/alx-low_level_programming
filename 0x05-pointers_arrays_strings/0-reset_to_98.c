bd206b2c07e25b0089c53e69df7bd226498e4323
Toyeisawesome <toyeisawesome@gmail.com>
2022-03-21 21:40:15 -0700

    reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include "main.h"
-
 /**
  * main - first task
  *
- * n: takes the value
+ * @n: takes the value
  *
  */
-void reset_to_98();
 void reset_to_98(int *n)
 {
 	int *n;
