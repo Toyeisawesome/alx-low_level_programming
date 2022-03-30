40dd7920e701863ed70c09e071feba9c91e20b22
Toyeisawesome <toyeisawesome@gmail.com>
2022-03-29 20:36:34 -0700

    strchr2

--- 0x07-pointers_arrays_strings/2-main.c
+++ 0x07-pointers_arrays_strings/2-main.c
@@ -17,24 +17,24 @@ int main(void)
 
 {
 
-	    char *s = "hello";
+    char *s = "hello";
 
-	        char *f;
-
-
-
-		    f = _strchr(s, 'l');
+    char *f;
 
 
 
-		        if (f != NULL)
+    f = _strchr(s, 'l');
 
-				    {
 
-					            printf("%s\n", f);
 
-						        }
+    if (f != NULL)
 
-			    return (0);
+    {
+
+            printf("%s\n", f);
+
+    }
+
+    return (0);
 
 }
