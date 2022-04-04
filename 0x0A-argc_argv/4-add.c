28545e427512d431f06b81b53142dca3c03c1b94
Toyeisawesome <toyeisawesome@gmail.com>
2022-04-04 06:17:47 -0700

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,9 +14,9 @@ int main(int argc, char *argv[])
 
 	sum = 0;
 
-	for (i = 1; i < argc; i++)
+	for (i = 1 ; i < argc ; i++)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		for (j = 0 ; argv[i][j] != '\0' ; j++)
 		{
 			if (argv[i][j] < 47 || argv[i][j] > 57)
 			{
